dffee85eb4b43abbb45c058be8fb938e3d05ce31
EvGen <evgen@evgen.com>
2012-11-03 09:58:18 +0300

    Найти все <img> теги

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 import urllib2
 import re
 
+patterns = {"a":"<a.*href=.*>.*</a>", "img":"<img.*src=.*>"}
+
 #TODO: url в параметре (запрос ввода) при запуске программы
 #TODO: Найти все <img> теги
 #TODO: Перенести get_page и search_tags в отдельный файл
@@ -11,27 +13,19 @@ def get_page(url):
     f = urllib2.urlopen(url)
     return f.read()
 
-def search_tags(page):
-    pattern = "<a.*href=.*>.*</a>"
+def search_tags(page, tag):
+    pattern = patterns[tag]
     template = re.compile(pattern)
     lst = template.findall(page)
     return lst
 
 def main():
     url = raw_input("Введите URL: ")
+    tag = raw_input("Тег (%s):" %  patterns.keys())
     page = get_page(url)
-    lst = search_tags(page)
-    print "count of <a> = %s" % len(lst)
+    lst = search_tags(page, tag)
     for l in lst:
         print l
 
 if __name__ == '__main__':
     main()
-
-
-
-
-
-
-
-
